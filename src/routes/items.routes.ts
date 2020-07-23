3a9ba82acf660141c309a45e728c897b5c929912
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-23 06:31:38 -0300

    Utilização Controller na rota de items

--- src/routes/items.routes.ts
+++ src/routes/items.routes.ts
@@ -1,63 +1,17 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
 
-import Item from '../models/Item';
-
-import CreateItemsService from '../services/CreateItemsService';
-import DeleteItemsService from '../services/DeleteItemsService';
-import UpdateItemsService from '../services/UpdateItemsService';
+import ItemsController from '../controllers/ItemsController';
 
 const itemsRouter = Router();
 
-itemsRouter.get('/', async (request, response) => {
-  const itemsRepository = getRepository(Item);
+const itemsController = new ItemsController();
 
-  const items = await itemsRepository.find();
-  return response.json(items);
-});
+itemsRouter.get('/', itemsController.index);
 
-itemsRouter.post('/', async (request, response) => {
-  const { name, price } = request.body;
+itemsRouter.post('/', itemsController.create);
 
-  const createItemsService = new CreateItemsService();
+itemsRouter.put('/:item_id', itemsController.update);
 
-  const item = await createItemsService.execute({
-    name,
-    price,
-  });
-
-  return response.json(item);
-});
-
-itemsRouter.put('/:item_id', async (request, response) => {
-  const { item_id } = request.params;
-  const { name, price } = request.body;
-  try {
-    const updateItemsService = new UpdateItemsService();
-
-    const itemUpdated = await updateItemsService.execute({
-      item_id,
-      name,
-      price,
-    });
-
-    return response.json(itemUpdated);
-  } catch (err) {
-    return response.status(400).json({ message: err.message });
-  }
-});
-
-itemsRouter.delete('/:item_id', async (request, response) => {
-  const { item_id } = request.params;
-  try {
-    const deleteItemsService = new DeleteItemsService();
-
-    await deleteItemsService.execute(item_id);
-
-    return response.status(204).json();
-  } catch (err) {
-    return response.status(400).json({ message: err.message });
-  }
-});
+itemsRouter.delete('/:item_id', itemsController.delete);
 
 export default itemsRouter;
