409bf9486cfd2b44c2456d4186d5e883f6d926c0
Emidio <52754546+vitorsemidio-dev@users.noreply.github.com>
2020-07-23 06:02:45 -0300

    CRUD Services (#5)
    * Preparação rotas para o CRUD
    
    * DeleteUser
    
    * Serviço para remoção de item
    
    * Atualização dados usuaŕio
    
    * Atualização items
    
    * ajustes ordençao importaçao

--- src/routes/items.routes.ts
+++ src/routes/items.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
 
-import CreateItemsService from '../services/CreateItemsService';
 import Item from '../models/Item';
+
+import CreateItemsService from '../services/CreateItemsService';
+import DeleteItemsService from '../services/DeleteItemsService';
+import UpdateItemsService from '../services/UpdateItemsService';
 
 const itemsRouter = Router();
 
@@ -24,6 +27,37 @@ itemsRouter.post('/', async (request, response) => {
   });
 
   return response.json(item);
+});
+
+itemsRouter.put('/:item_id', async (request, response) => {
+  const { item_id } = request.params;
+  const { name, price } = request.body;
+  try {
+    const updateItemsService = new UpdateItemsService();
+
+    const itemUpdated = await updateItemsService.execute({
+      item_id,
+      name,
+      price,
+    });
+
+    return response.json(itemUpdated);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+});
+
+itemsRouter.delete('/:item_id', async (request, response) => {
+  const { item_id } = request.params;
+  try {
+    const deleteItemsService = new DeleteItemsService();
+
+    await deleteItemsService.execute(item_id);
+
+    return response.status(204).json();
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
 });
 
 export default itemsRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
 
-import CreateUsersService from '../services/CreateUsesrService';
-
 import User from '../models/User';
+
+import CreateUsersService from '../services/CreateUsersService';
+import DeleteUsersService from '../services/DeleteUsersService';
+import UpdateUsersService from '../services/UpdateUsersService';
 
 const usersRouter = Router();
 
@@ -27,6 +29,38 @@ usersRouter.post('/', async (request, response) => {
     });
 
     return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+});
+
+usersRouter.put('/:user_id', async (request, response) => {
+  const { user_id } = request.params;
+  const { name, email, password } = request.body;
+  try {
+    const updateUsersService = new UpdateUsersService();
+
+    const userUpdated = await updateUsersService.execute({
+      user_id,
+      name,
+      email,
+      password,
+    });
+
+    return response.json(userUpdated);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+});
+
+usersRouter.delete('/:user_id', async (request, response) => {
+  const { user_id } = request.params;
+  try {
+    const deleteUsersService = new DeleteUsersService();
+
+    await deleteUsersService.execute(user_id);
+
+    return response.status(204).json();
   } catch (err) {
     return response.status(400).json({ message: err.message });
   }
