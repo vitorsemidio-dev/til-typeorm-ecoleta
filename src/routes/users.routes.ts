05b7867a15bb00235999138f06f17e83d47c44c5
Emidio <52754546+vitorsemidio-dev@users.noreply.github.com>
2020-07-23 04:54:29 -0300

    Inserção dados no banco de dados utilizando services (#4)
    * Criação CreateUsersService
    
    * Criação usuário e aplicação aceitando body json
    
    * Criação CreateItemsService
    
    * Validação e-mail já registrado

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
 
+import CreateUsersService from '../services/CreateUsesrService';
+
 import User from '../models/User';
 
 const usersRouter = Router();
@@ -11,6 +13,23 @@ usersRouter.get('/', async (request, response) => {
   const users = await usersRepository.find();
 
   return response.json(users);
+});
+
+usersRouter.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+  try {
+    const createUsersService = new CreateUsersService();
+
+    const user = await createUsersService.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
 });
 
 export default usersRouter;
---
+++ src/services/CreateUsesrService.ts
@@ -0,0 +1,37 @@
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUsersService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkEmail = await usersRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (checkEmail) {
+      throw new Error('E-mail already used');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUsersService;
