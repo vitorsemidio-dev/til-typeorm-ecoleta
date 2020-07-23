f540347012f35e14b9fe11654aa5af394dc95fd4
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-23 04:34:32 -0300

    Criação CreateUsersService

---
+++ src/services/CreateUsesrService.ts
@@ -0,0 +1,27 @@
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUsersService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUsersService;
