5813690ee358750ef8f1dd0aa39cc2cbd5430be6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-25 02:16:13 -0300

    Teste: Feito cenários de Update Users Service

--- src/services/UpdateUsersService.spec.ts
+++ src/services/UpdateUsersService.spec.ts
@@ -1,13 +1,16 @@
+import { uuid } from 'uuidv4';
 import { Connection, getConnection } from 'typeorm';
 
 import createConnection from '../database';
 
+import CreateUsersService from './CreateUsersService';
 import UpdateUsersService from './UpdateUsersService';
 
 const connectionName = 'default';
 
 let connection: Connection;
 
+let createUsersService: CreateUsersService;
 let updateUsersService: UpdateUsersService;
 
 describe('Update Users Service', () => {
@@ -24,6 +27,7 @@ describe('Update Users Service', () => {
   beforeEach(async () => {
     await connection.query('DELETE FROM users');
 
+    createUsersService = new CreateUsersService();
     updateUsersService = new UpdateUsersService();
   });
 
@@ -35,14 +39,60 @@ describe('Update Users Service', () => {
   });
 
   it('should be able to update user', async () => {
-    // TODO
+    const user = await createUsersService.execute({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const userUpdated = await updateUsersService.execute({
+      user_id: user.id,
+      name: 'Remi',
+      email: 'remi@sandstorm.com',
+      password: '123456',
+    });
+
+    expect(userUpdated.id).toEqual(user.id);
+    expect(userUpdated).toMatchObject({
+      name: 'Remi',
+      email: 'remi@sandstorm.com',
+      password: '123456',
+    });
   });
 
   it('should not be able to update email if it is already used by another user', async () => {
-    // TODO
+    await createUsersService.execute({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const { id } = await createUsersService.execute({
+      name: 'Remi',
+      email: 'remi@sandstorm.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateUsersService.execute({
+        user_id: id,
+        name: 'Remi',
+        email: 'janedoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(Error);
   });
 
   it('should not be able to update data from a non-existing user', async () => {
-    // TODO
+    const userId = uuid();
+
+    await expect(
+      updateUsersService.execute({
+        user_id: userId,
+        name: 'Non-existing user',
+        email: 'non-existing-user@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(Error);
   });
 });
