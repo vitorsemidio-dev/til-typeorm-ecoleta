d9066fa86339bfd74a86a22a4d12d4d6f03d57bb
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-25 00:44:37 -0300

    Teste: Cenários de testes para UpdateItemsService

---
+++ src/services/UpdateItemsService.spec.ts
@@ -0,0 +1,15 @@
+import UpdateItemsService from './UpdateItemsService';
+
+describe('Update Items Service', () => {
+  it('should be able to update an item', async () => {
+    // TODO
+  });
+
+  it('should not be able to update a non-existing item', async () => {
+    // TODO
+  });
+
+  it('should not be able to update an item with a negative price', async () => {
+    // TODO
+  });
+});
