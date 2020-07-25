1c23745dfadef9fea7e6ac78de22f7830b62ccfb
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-24 22:12:27 -0300

    Remoçaõ teste fake

--- src/services/first-test.spec.ts
+++
@@ -1,9 +0,0 @@
-import sum from './first-test';
-
-describe('first test', () => {
-  it('should test if jest config is working for the first fake test', () => {
-    const sumNumbers = sum(3, 5);
-
-    expect(sumNumbers).toBe(8);
-  });
-});
--- src/services/first-test.ts
+++
@@ -1,6 +0,0 @@
-export default function sum(a: number, b: number): number {
-  if (a === 2 && b === 2) {
-    return a * b;
-  }
-  return a + b;
-}
