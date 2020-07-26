e377e089c0968ae49def74cf7cb94a6d6e80a342
Emidio <52754546+vitorsemidio-dev@users.noreply.github.com>
2020-07-26 14:20:03 -0300

    Testes das rotas de usuário e item (#8)
    * Teste: Primeiro teste de rota
    
    * Teste: Listagem de usuários
    
    * Teste: Listagem de items
    
    * eslint import/no-extraneous-dependencies off
    
    * Config para TODO Highlight
    
    * Teste Rotas: Cenarios para rotas de usuarios
    
    * Teste Rotas: Cenarios para rotas de Item
    
    * Teste Rotas: Criaçao usuario
    
    * Teste Rotas: Atualização todas informações do usuário e atualização uma por uma
    
    * Teste Rotas: Atualização usuário
    Não permitir atualização para um email já usado
    Não permitir atualização de usuário inexistente
    
    * Teste Rotas: Remoção usuário
    
    * Refatoração nome dos arquivos
    
    * Teste Rotas: Criaçao item
    
    * Teste Rotas: Atualização Item
    
    * Teste Rotas: Remoção item
    
    * Anotação sobre falha nos testes

---
+++ src/routes/items.routes.spec.ts
@@ -0,0 +1,189 @@
+import request from 'supertest';
+import { Connection, getConnection } from 'typeorm';
+import { uuid } from 'uuidv4';
+
+import app from '../app';
+import createConnection from '../database';
+
+const connectionName = 'test-connection';
+
+let connection: Connection;
+
+describe('Routes Test', () => {
+  beforeAll(async () => {
+    connection = await createConnection(connectionName);
+
+    await connection.query('DROP TABLE IF EXISTS items');
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP TABLE IF EXISTS migrations');
+
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM users');
+  });
+
+  afterAll(async () => {
+    const mainConnection = getConnection();
+
+    await connection.close();
+    await mainConnection.close();
+  });
+
+  // List items
+  it('should be able to items', async () => {
+    const {
+      body: { id: idItemA },
+    } = await request(app).post('/items').send({
+      name: 'Item A',
+      price: 10,
+    });
+
+    const {
+      body: { id: idItemB },
+    } = await request(app).post('/items').send({
+      name: 'Item B',
+      price: 20,
+    });
+
+    const {
+      body: { id: idItemC },
+    } = await request(app).post('/items').send({
+      name: 'Item C',
+      price: 30,
+    });
+
+    const response = await request(app).get('/items');
+
+    expect(response.body).toHaveLength(3);
+
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: 'Item A',
+          id: idItemA,
+        }),
+        expect.objectContaining({
+          name: 'Item B',
+          id: idItemB,
+        }),
+        expect.objectContaining({
+          name: 'Item C',
+          id: idItemC,
+        }),
+      ]),
+    );
+  });
+
+  // Create item
+  it('should be able to create a new item', async () => {
+    const responseCreateItem = await request(app).post('/items').send({
+      name: 'Item',
+      price: 1,
+    });
+
+    expect(responseCreateItem.status).toBe(200);
+    expect(responseCreateItem.body).toHaveProperty('id');
+    expect(responseCreateItem.body).toMatchObject(
+      expect.objectContaining({
+        name: 'Item',
+        price: 1,
+      }),
+    );
+  });
+
+  it('should not be able to create a new item with a negative price', async () => {
+    // TEST RED: Create RN on service
+    const responseCreateItem = await request(app).post('/items').send({
+      name: 'Item',
+      price: -1,
+    });
+
+    expect(responseCreateItem.status).toBe(400);
+
+    expect(responseCreateItem.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+  });
+
+  // Update item
+  // TODO update without await is breaking some tests
+  it('should be able to update an item', async () => {
+    // TEST RED: await missing when call execute function
+    const {
+      body: { id },
+    } = await request(app).post('/items').send({
+      name: 'Item',
+      price: 1,
+    });
+
+    const responseUpdateItem = await request(app).put(`/items/${id}`).send({
+      name: 'Item update',
+      price: 100,
+    });
+
+    expect(responseUpdateItem.status).toBe(200);
+    expect(responseUpdateItem.body).toHaveProperty('id');
+
+    expect(responseUpdateItem.body).toMatchObject({
+      id,
+      name: 'Item update',
+      price: 100,
+    });
+  });
+
+  it('should not be able to update a non-existing item', async () => {
+    // TEST RED: await missing when call execute function
+    const id = uuid();
+
+    const responseUpdateItem = await request(app).put(`/items/${id}`).send({
+      name: 'Item',
+      price: 1,
+    });
+
+    expect(responseUpdateItem.status).toBe(400);
+  });
+
+  it('shoul not be able to update an item with a negative price', async () => {
+    // TEST RED: missing validate negative price
+    const {
+      body: { id },
+    } = await request(app).post('/items').send({
+      name: 'Item',
+      price: 1,
+    });
+
+    const responseUpdateItem = await request(app).put(`/items/${id}`).send({
+      name: 'Item update',
+      price: -1,
+    });
+
+    expect(responseUpdateItem.status).toBe(400);
+  });
+
+  // Delete item
+  it('should be able to delete an item', async () => {
+    const {
+      body: { id },
+    } = await request(app).post('/items').send({
+      name: 'Item',
+      price: 1,
+    });
+
+    const responseDeleteItem = await request(app).delete(`/items/${id}`);
+
+    expect(responseDeleteItem.status).toBe(204);
+    expect(responseDeleteItem.body).toMatchObject({});
+  });
+
+  it('should not be able to delete an item', async () => {
+    const id = uuid();
+
+    const responseDeleteItem = await request(app).delete(`/items/${id}`);
+
+    expect(responseDeleteItem.status).toBe(400);
+  });
+});
---
+++ src/routes/users.routes.spec.ts
@@ -0,0 +1,312 @@
+import request from 'supertest';
+import { Connection, getConnection } from 'typeorm';
+import { uuid } from 'uuidv4';
+
+import app from '../app';
+import createConnection from '../database';
+
+const connectionName = 'test-connection';
+
+let connection: Connection;
+
+describe('Routes Test', () => {
+  beforeAll(async () => {
+    connection = await createConnection(connectionName);
+
+    await connection.query('DROP TABLE IF EXISTS items');
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP TABLE IF EXISTS migrations');
+
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM users');
+  });
+
+  afterAll(async () => {
+    const mainConnection = getConnection();
+
+    await connection.close();
+    await mainConnection.close();
+  });
+
+  // List users
+  it('should be able to list users', async () => {
+    const {
+      body: { id: idJane },
+    } = await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const {
+      body: { id: idAlice },
+    } = await request(app).post('/users').send({
+      name: 'Alice Kruger',
+      email: 'alicekruger@example.com',
+      password: '123456',
+    });
+
+    const {
+      body: { id: idRemi },
+    } = await request(app).post('/users').send({
+      name: 'Remi',
+      email: 'remi@sandstorm.com',
+      password: '123456',
+    });
+
+    const response = await request(app).get('/users');
+
+    expect(response.body).toHaveLength(3);
+
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: 'Jane Doe',
+          id: idJane,
+        }),
+        expect.objectContaining({
+          name: 'Alice Kruger',
+          id: idAlice,
+        }),
+        expect.objectContaining({
+          name: 'Remi',
+          id: idRemi,
+        }),
+      ]),
+    );
+  });
+
+  // Create User
+  it('should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toMatchObject({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+  });
+
+  it('should not be able to create a new user with a e-mail already registred', async () => {
+    await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const response = await request(app).post('/users').send({
+      name: 'Remi',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+  });
+
+  it('should not be able to create a new user with an invalid e-mail', async () => {
+    // TEST RED:
+    const response = await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'invalida-email',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+  });
+
+  it('should not be able to create a new user without some information (name, email, password)', async () => {
+    // TEST GREEN: null value violates not-null constraint
+    const responseWithoutName = await request(app).post('/users').send({
+      email: 'withoutname@example.com',
+      password: '123456',
+    });
+
+    const responseWithoutEmail = await request(app).post('/users').send({
+      name: 'Jane Doe',
+      password: '123456',
+    });
+
+    const responseWithoutPassword = await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'withoutpassword@example.com',
+    });
+
+    expect(responseWithoutName.status).toBe(400);
+    expect(responseWithoutEmail.status).toBe(400);
+    expect(responseWithoutPassword.status).toBe(400);
+
+    expect(responseWithoutName.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+
+    expect(responseWithoutEmail.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+
+    expect(responseWithoutPassword.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+  });
+
+  // Update User
+  it('should be able to update all user information', async () => {
+    const responseCreateUser = await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    expect(responseCreateUser.body).toHaveProperty('id');
+
+    const { id } = responseCreateUser.body;
+
+    const responseUpdateUser = await request(app).put(`/users/${id}`).send({
+      name: 'Remi',
+      email: 'remi@example',
+      password: '654321',
+    });
+
+    expect(responseUpdateUser.body).toHaveProperty('id');
+    expect(responseUpdateUser.body.id).toEqual(responseCreateUser.body.id);
+    expect(responseUpdateUser.body).toMatchObject({
+      name: 'Remi',
+      email: 'remi@example',
+      password: '654321',
+    });
+  });
+
+  it('should be able to update each data from user', async () => {
+    // TEST RED: Fix service emptying not provider's data
+    const {
+      body: { id },
+    } = await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const responseUpdateName = await request(app).put(`/users/${id}`).send({
+      name: 'Remi',
+    });
+
+    expect(responseUpdateName.status).toBe(200);
+    expect(responseUpdateName.body).toMatchObject({
+      id,
+      name: 'Remi',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const responseUpdateEmail = await request(app).put(`/users/${id}`).send({
+      email: 'remi@sandstorm.com',
+    });
+
+    expect(responseUpdateEmail.status).toBe(200);
+    expect(responseUpdateEmail.body).toMatchObject({
+      id,
+      name: 'Remi',
+      email: 'remi@sandstorn.com',
+      password: '123456',
+    });
+
+    const responseUpdatePassword = await request(app).put(`/users/${id}`).send({
+      password: '654321',
+    });
+
+    expect(responseUpdatePassword.status).toBe(200);
+    expect(responseUpdatePassword.body).toMatchObject({
+      id,
+      name: 'Remi',
+      email: 'remi@sandstorm.com',
+      password: '654321',
+    });
+  });
+
+  it('should not be able to update e-mail to another one that is already used', async () => {
+    await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const responseUser = await request(app).post('/users').send({
+      name: 'Remi',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    expect(responseUser.status).toBe(400);
+    expect(responseUser.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+  });
+
+  it('should not be able to update a non-existing user', async () => {
+    const id = uuid();
+    const response = await request(app).put(`/users/${id}`).send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+  });
+
+  // Delete user
+  it('should be able to delete an user', async () => {
+    const {
+      body: { id },
+    } = await request(app).post('/users').send({
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      password: '123456',
+    });
+
+    const responseDeleteUser = await request(app).delete(`/users/${id}`);
+
+    expect(responseDeleteUser.status).toBe(204);
+  });
+
+  it('should not be able to delete a non-existing user', async () => {
+    const id = uuid();
+    const responseDeleteUser = await request(app).delete(`/users/${id}`);
+
+    expect(responseDeleteUser.status).toBe(400);
+    expect(responseDeleteUser.body).toMatchObject(
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    );
+  });
+});
